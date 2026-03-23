bd16b23889eafe303319385f7a014c5541d31138
Ray <xurui93@qq.com>
2026-03-23 17:10:34 +0800

    dep: 升级依赖
    Signed-off-by: Ray <xurui93@qq.com>

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: join(__dirname),
+  outputFileTracingRoot: __dirname,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
